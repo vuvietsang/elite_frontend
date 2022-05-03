7bd16ba74d224d3dfdcefc75e8639b29a417a20e
Sang <vuvietsang10a9@gmail.com>
2022-05-03 17:28:44 +0700

    setup template admin

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,33 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
-import Home from './screens/Home'
-import { Route, Routes } from 'react-router-dom'
-import Cart from './screens/Cart'
-import Header from './components/Header'
-import Footer from './components/Footer'
-import Contact from './screens/Contact'
-import Checkout from './screens/Checkout'
-import ProductDetails from './screens/ProductDetails'
-import Products from './screens/Products'
+import { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Home from "./screens/Home";
+import { Route, Routes } from "react-router-dom";
+import Cart from "./screens/Cart";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Contact from "./screens/Contact";
+import Checkout from "./screens/Checkout";
+import ProductDetails from "./screens/ProductDetails";
+import Products from "./screens/Products";
+import Admin from "./screens/Admin";
 
 function App() {
-
   return (
     <div className="App">
-      <Header/>
+      <Header />
       <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='/cart' element={<Cart/>}/>
-      <Route path='/contact' element={<Contact/>}/>
-      <Route path='/checkout' element={<Checkout/>}/>
-      <Route path="/details/:id" element={<ProductDetails/>}/>
-      <Route path='/products' element={<Products/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/details/:id" element={<ProductDetails />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/admin" element={<Admin />} />
       </Routes>
-      <Footer/>
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
